31ca129ad4f9b5cfb63010b6e469f26c9aac75e8
cmsunflower <2356467938@qq.com>
2020-02-27 21:37:40 +0800

    清空status

--- config/index.js
+++ config/index.js
@@ -10,7 +10,14 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // '/api':{
+      //   target:'https://www.liulongbin.top:8888',
+      //   changeOrigin:true,
+      //   pathRewrite:{
+      //     '^/api':''
+      //   }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
